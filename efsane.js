3d98928f6739caf6d5bf22bd6c4fac288de9e6ab
omerhazir <omerhazir@hotmail.com>
2017-04-04 20:23:21 +0300

    Update efsane.js

--- efsane.js
+++ efsane.js
@@ -1,7 +1,6 @@
-		$(function() {
-            _x_canvas = document.getElementById("canvas");
-			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
-			$("#connect").click(function() {a.core.connect($("#server").val()) });
-			$("#reconnect").click(function() { MC.reconnect()})
-			
+	$(function() {
+       _x_canvas = document.getElementById("canvas");
+			$("#gamemode").after('<br><br><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
+			$("#reconnect").click(function() {MC.reconnect() })
+		
         })
