68de4a722f2db55738359d17eea7c2d6f0978c04
omerhazir <omerhazir@hotmail.com>
2017-04-04 19:53:42 +0300

    Create efsane.js

---
+++ efsane.js
@@ -0,0 +1,8 @@
+	$(function() {
+       _x_canvas = document.getElementById("canvas");
+			$("#gamemode").after('<br><br><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
+			$("#reconnect").click(function() {
+                MC.reconnect()
+            })
+		
+        })
