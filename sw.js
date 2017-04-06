2bcc46a2cbf6aa518608a51e60c9aeaca739f014
omerhazir <omerhazir@hotmail.com>
2017-04-06 19:50:03 +0300

    Update and rename efsane.js to sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
+// 676
+// _x_start
+//var _x_input;
+//var _x_chatWebsocket;
+// _x_end
 (function(a, c) {
     function r(a, d) {
         if (d) {
@@ -8,7 +13,24 @@
         document.cookie = "agario_redirect=" + a + f + "; path=/"
     }
 
- 
+    /*function N() {
+        for (var a = document.cookie.split(";"), d = 0; d < a.length; d++) {
+            for (var f = a[d];
+                " " == f.charAt(0);) f = f.substring(1, f.length);
+            if (0 == f.indexOf("agario_redirect=")) return f.substring(16, f.length)
+        }
+        return null
+    }
+
+    /*function O() {
+        c.get(P + "//gc.agar.io", function(a) {
+            var d = a.split(" ");
+            a = d[0];
+            d = d[1] || "";
+            q.hasOwnProperty(a) && ("string" == typeof q[a] ? MC.getRegion() || MC.setRegion(q[a]) : q[a].hasOwnProperty(d) && (MC.getRegion() || MC.setRegion(q[a][d])))
+        }, "text")
+    }*/
+		// _x_start
 		$(function() {
             _x_canvas = document.getElementById("canvas");
 			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
@@ -18,7 +40,8 @@
 			$("#reconnect").click(function() {
                 MC.reconnect()
             })
-
+			/*
+			*/
         })
 		// _x_end
 
