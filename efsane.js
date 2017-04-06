801c2c9307464e545083234f1326f3bfeb9c8a67
omerhazir <omerhazir@hotmail.com>
2017-04-06 19:36:01 +0300

    Update efsane.js

--- efsane.js
+++ efsane.js
@@ -1,8 +1,3 @@
-// 676
-// _x_start
-//var _x_input;
-//var _x_chatWebsocket;
-// _x_end
 (function(a, c) {
     function r(a, d) {
         if (d) {
@@ -12,24 +7,7 @@
         } else f = "";
         document.cookie = "agario_redirect=" + a + f + "; path=/"
     }
-
-    /*function N() {
-        for (var a = document.cookie.split(";"), d = 0; d < a.length; d++) {
-            for (var f = a[d];
-                " " == f.charAt(0);) f = f.substring(1, f.length);
-            if (0 == f.indexOf("agario_redirect=")) return f.substring(16, f.length)
-        }
-        return null
-    }
-
-    /*function O() {
-        c.get(P + "//gc.agar.io", function(a) {
-            var d = a.split(" ");
-            a = d[0];
-            d = d[1] || "";
-            q.hasOwnProperty(a) && ("string" == typeof q[a] ? MC.getRegion() || MC.setRegion(q[a]) : q[a].hasOwnProperty(d) && (MC.getRegion() || MC.setRegion(q[a][d])))
-        }, "text")
-    }*/
+})
 		// _x_start
 		$(function() {
             _x_canvas = document.getElementById("canvas");
@@ -44,5 +22,3 @@
 			*/
         })
 		// _x_end
-
-})(window, window.jQuery);
