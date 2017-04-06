c47a389e9fcbae08daacae3c234053d1be9f2060
omerhazir <omerhazir@hotmail.com>
2017-04-06 19:40:51 +0300

    Update efsane.js

--- efsane.js
+++ efsane.js
@@ -8,7 +8,7 @@
         document.cookie = "agario_redirect=" + a + f + "; path=/"
     }
 })
-		// _x_start
+		// start
 		$(function() {
             _x_canvas = document.getElementById("canvas");
 			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
@@ -18,7 +18,5 @@
 			$("#reconnect").click(function() {
                 MC.reconnect()
             })
-			/*
-			*/
         })
 		// _x_end
