659c895da7fe9427ad3aafd195698cd119455eed
omerhazir <omerhazir@hotmail.com>
2017-04-08 20:36:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
         document.cookie = "agario_redirect=" + a + f + "; path=/"
     }
 		// _x_start
-		$(function() {
+		$(function() 
 			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
 			$("#connect").click(function() {
                 a.core.connect($("#server").val())
@@ -17,7 +17,15 @@
              MC.reconnect()
              adres();
             })
-        })
+$( "#region" ).on('change', function() {
+ MC.setRegion($('#region').val());   
+  adres();
+});
+           
+$('#gamemode').on('change', function() {
+  adres();
+});            
+        });
         // _x_end  
    adres();         
 function adres() {
