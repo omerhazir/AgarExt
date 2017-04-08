c552dfa185aa7a132e48d341e89a07ab515c836b
omerhazir <omerhazir@hotmail.com>
2017-04-08 20:40:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,36 +7,23 @@
         } else f = "";
         document.cookie = "agario_redirect=" + a + f + "; path=/"
     }
+
+
+
+
 		// _x_start
-		$(function() 
+		$(function() {
+            _x_canvas = document.getElementById("canvas");
 			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
 			$("#connect").click(function() {
                 a.core.connect($("#server").val())
             });
 			$("#reconnect").click(function() {
-             MC.reconnect()
-             adres();
+                MC.reconnect()
             })
-$( "#region" ).on('change', function() {
- MC.setRegion($('#region').val());   
-  adres();
-});
-           
-$('#gamemode').on('change', function() {
-  adres();
-});            
+			/*
+			*/
         })
-        // _x_end  
-   adres();         
-function adres() {
-    var adrs = WebSocket.prototype.send;
-    window.__WS_send = WebSocket.prototype.send, WebSocket.prototype.send = function(b) {
-         $("#server").val(this.url);
-        try {
-            adrs.apply(this, [b]), WebSocket.prototype.send = adrs
-        } catch (e) {
-            window.__WS_send.apply(this, [b]), WebSocket.prototype.send = window.__WS_send
-        }
-    }
-}
+		// _x_end
+
 })(window, window.jQuery);
