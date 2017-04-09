7513d93285f0cd47a00321b23d17fb05afe905ad
omerhazir <omerhazir@hotmail.com>
2017-04-09 13:44:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,9 +13,7 @@
 
 		// _x_start
 		$(function() {
-                        $( "#leaderboard-hud" ).append( '<div style="statsText" id="swyaz" title="Kopyalamak için tıkla" style="cursor: pointer;"></div>' );
-			//$( ".leaderboard-panel" ).append( "<div>deneme</div>" );
-			//$( "#leaderboard-hud" ).append( "<div>deneme</div>" );
+                        $( "#leaderboard-hud" ).append( '<div id="swyaz" title="Kopyalamak için tıkla" style="cursor: pointer;"></div>' );
 			$("#gamemode").after('<br><br><input id="server" class="form-control" style="width: 59%;  display: inline-block; margin-right: 5px"><button type="submit" id="connect" class="btn btn-primary" style="width: 25%; display: inline-block; margin-right: 5px">Connect</button><button type="button" id="reconnect" class="btn btn-info" style="display: inline-block"><i class="glyphicon glyphicon-refresh"></i></button>');
 			$("#swyaz").click(function() {
                 		var temp = document.createElement("input");
