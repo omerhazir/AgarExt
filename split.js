b9f1268cd1ed3f3fd5f968399a0fac86a230637e
omerhazir <omerhazir@hotmail.com>
2017-05-18 22:08:33 +0300

    Update split.js

--- split.js
+++ split.js
@@ -1,25 +1,23 @@
 window.addEventListener('keydown', keydown);
 window.addEventListener('keyup', keyup);
-var Feed = false;
-var Dingus = false;
 var imlost = 25;
 function keydown(event) {
- // Tricksplit E
+ // Tricksplit 4
     if (event.keyCode == 52) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
         setTimeout(ilikedick, imlost*3);
-    } // Triplesplit Z
+    } // Triplesplit 3
     if (event.keyCode == 51) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
-    } // Doublesplit D
+    } // Doublesplit 2
     if (event.keyCode == 50) {
         ilikedick();
         setTimeout(ilikedick, imlost);
-    } // PopSplit R
+    } // PopSplit 1
     if (event.keyCode == 49) {
         ilikedick();
         setTimeout(ilikedick, imlost*8.2142354224);
@@ -31,4 +29,3 @@ function ilikedick() {
     $("body").trigger($.Event("keydown", { keyCode: 32}));
     $("body").trigger($.Event("keyup", { keyCode: 32}));
 }
-//Please don't switch the code { u know why ( ?° ?? ?°) }
