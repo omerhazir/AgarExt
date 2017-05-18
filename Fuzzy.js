166fa4fc2bc25f8e7dc97b36e453d6d4176b555c
omerhazir <omerhazir@hotmail.com>
2017-05-18 22:04:27 +0300

    Update Fuzzy.js

--- Fuzzy.js
+++ Fuzzy.js
@@ -5,8 +5,7 @@ function keydown(event) {
  // Triplesplit Z
     if (event.keyCode == 68) {
         ilikedick();
-        setTimeout(ilikedick, imlost);
-        setTimeout(ilikedick, imlost*2);
+        setTimeout(ilikedick, imlost*3);
     }
 }
 
