a15a8dcf8b19d4cf208d191eadddc5ab8c0cd3f9
omerhazir <omerhazir@hotmail.com>
2017-05-18 22:07:09 +0300

    Update split.js

--- split.js
+++ split.js
@@ -5,22 +5,22 @@ var Dingus = false;
 var imlost = 25;
 function keydown(event) {
  // Tricksplit E
-    if (event.keyCode == 49) {
+    if (event.keyCode == 52) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
         setTimeout(ilikedick, imlost*3);
     } // Triplesplit Z
-    if (event.keyCode == 50) {
+    if (event.keyCode == 51) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
     } // Doublesplit D
-    if (event.keyCode == 51) {
+    if (event.keyCode == 50) {
         ilikedick();
         setTimeout(ilikedick, imlost);
     } // PopSplit R
-    if (event.keyCode == 52) {
+    if (event.keyCode == 49) {
         ilikedick();
         setTimeout(ilikedick, imlost*8.2142354224);
     }
