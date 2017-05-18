d427c2b66c8dce45cd01a1c412afe3c0d83fe5ce
omerhazir <omerhazir@hotmail.com>
2017-05-18 20:49:18 +0300

    Update Fuzzy.js

--- Fuzzy.js
+++ Fuzzy.js
@@ -3,7 +3,7 @@ window.addEventListener('keyup', keyup);
 var imlost = 25;
 function keydown(event) {
  // Triplesplit Z
-    if (event.keyCode == 49) {
+    if (event.keyCode == 68) {
         ilikedick();
         setTimeout(ilikedick, imlost);
         setTimeout(ilikedick, imlost*2);
