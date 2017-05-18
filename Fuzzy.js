25b228994bca424ebb6bb2a61d4010652877f17b
omerhazir <omerhazir@hotmail.com>
2017-05-18 20:44:24 +0300

    Create Fuzzy.js

---
+++ Fuzzy.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown', keydown);
+window.addEventListener('keyup', keyup);
+var imlost = 25;
+function keydown(event) {
+ // Triplesplit Z
+    if (event.keyCode == 49) {
+        ilikedick();
+        setTimeout(ilikedick, imlost);
+        setTimeout(ilikedick, imlost*2);
+    }
+}
+
+function ilikedick() {
+    $("body").trigger($.Event("keydown", { keyCode: 32}));
+    $("body").trigger($.Event("keyup", { keyCode: 32}));
+}
